6fdd8517c1bbcfc0743230a28f571a0d85a74d7e
ncusimano <cusimano@ualberta.ca>
2023-03-01 15:19:18 -0700

    Reset App to Include Resources Folder
    Resource folder and default build was missing from empty application. Made a fresh start of the project to add a default empty layout and the associated default infill for a new project so we aren't starting from scratch.

--- app/src/androidTest/java/com/example/qrranger/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/example/qrranger/ExampleInstrumentedTest.java
@@ -1,4 +1,4 @@
-package com.example.qr_ranger;
+package com.example.qrranger;
 
 import android.content.Context;
 
@@ -21,6 +21,6 @@ public class ExampleInstrumentedTest {
     public void useAppContext() {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
-        assertEquals("com.example.qr_ranger", appContext.getPackageName());
+        assertEquals("com.example.qrranger", appContext.getPackageName());
     }
 }

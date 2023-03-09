9b119904964db302117d35d706f4b46020fc9ba9
Benny <BenCode.git@gmail.com>
2023-03-09 01:14:40 -0700

    map ui only search view + picture

--- app/src/main/java/com/example/qrranger/MapFragment.java
+++ app/src/main/java/com/example/qrranger/MapFragment.java
@@ -7,6 +7,7 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.SearchView;
 
 
 public class MapFragment extends Fragment {
--- app/src/main/java/com/example/qrranger/ProfileFragment.java
+++ app/src/main/java/com/example/qrranger/ProfileFragment.java
@@ -1,5 +1,7 @@
 package com.example.qrranger;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -7,12 +9,10 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 public class ProfileFragment extends Fragment {
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_profile, container, false);
-    }
 }

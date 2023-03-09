fb84f55689e8d5c352ea960702d3c9a98fc09ea3
Benny <BenCode.git@gmail.com>
2023-03-09 03:10:23 -0700

    all xml set up just to link the stuff up

--- app/src/main/java/com/example/qrranger/SearchFragment.java
+++ app/src/main/java/com/example/qrranger/SearchFragment.java
@@ -7,13 +7,29 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.SearchView;
 
 public class SearchFragment extends Fragment {
+    SearchView SearchUser;
+    ListView Users;
+    ArrayAdapter<String> UserAdapter;
+    // just test delete when database connected
+    String[] random = {"a", "b"};
+    public SearchFragment() {
+    }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_search, container, false);
+        View view = inflater.inflate(R.layout.fragment_search, container, false);
+        Users = (ListView) view.findViewById(R.id.search_users_list);
+        UserAdapter = new ArrayAdapter<>(getActivity(), android.R.layout.simple_list_item_1,random);
+        Users.setAdapter(UserAdapter);
+        return view;
+
     }
+
 }

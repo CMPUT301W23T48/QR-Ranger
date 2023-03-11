34f4b72eba2f94466ac8b7f913a5eaa6f8596193
Benny <BenCode.git@gmail.com>
2023-03-11 13:09:03 -0700

    first ui push

--- app/src/main/java/com/example/qrranger/SearchFragment.java
+++ app/src/main/java/com/example/qrranger/SearchFragment.java
@@ -1,20 +1,18 @@
 package com.example.qrranger;
 
 import android.app.Activity;
-import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
-import androidx.fragment.app.FragmentManager;
-import androidx.fragment.app.FragmentTransaction;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.SearchView;
+
+import com.example.qrranger.R;
 
 import java.util.ArrayList;
 

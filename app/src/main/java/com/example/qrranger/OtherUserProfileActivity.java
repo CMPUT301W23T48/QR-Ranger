494be1f4b9195b62a805f2785e99c9055810d1e4
Adam Boman <adamboman1@hotmail.com>
2023-03-12 15:21:12 -0600

    missed a file

--- app/src/main/java/com/example/qrranger/OtherUserProfileActivity.java
+++ app/src/main/java/com/example/qrranger/OtherUserProfileActivity.java
@@ -9,34 +9,35 @@ import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import java.util.concurrent.CompletableFuture;
+
 public class OtherUserProfileActivity extends AppCompatActivity {
 
-    private Button backButton;
-    private TextView usernameView;
-    private TextView userEmailView;
-    private TextView userPhoneNumberView;
-    private TextView rankView;
-    private TextView totalScoreView;
-    private TextView totalQRNumView;
-
-    private Intent intent = getIntent();
+    Button backButton;
+    TextView usernameView;
+    TextView userEmailView;
+    TextView userPhoneNumberView;
+    TextView rankView;
+    TextView totalScoreView;
+    TextView totalQRNumView;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_otheruser);
+        Intent intent = getIntent();
 
         LinearLayout linearLayout = findViewById(R.id.OtherUserLL1);
         backButton = linearLayout.findViewById(R.id.OUBackButton);
         usernameView = linearLayout.findViewById(R.id.OUUserName);
-        userEmailView = linearLayout.findViewById(R.id.OUEmail);
-        userPhoneNumberView = linearLayout.findViewById(R.id.OUPhoneNumber);
+        userEmailView = linearLayout.findViewById(R.id.OUUserEmail);
+        userPhoneNumberView = linearLayout.findViewById(R.id.OUUserPhoneNumber);
         rankView = linearLayout.findViewById(R.id.OURank);
         totalScoreView = linearLayout.findViewById(R.id.OUTS);
         totalQRNumView = linearLayout.findViewById(R.id.OUQRNum);
 
-        setViews();
+        setViews(intent);
 
         backButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -47,7 +48,24 @@ public class OtherUserProfileActivity extends AppCompatActivity {
 
     }
 
-    public void setViews(){
-        
+    public void setViews(Intent intent) {
+        usernameView.setText(intent.getStringExtra("username"));
+        userEmailView.setText(intent.getStringExtra("email"));
+        userPhoneNumberView.setText(intent.getStringExtra("phoneNumber"));
+        totalScoreView.setText(intent.getStringExtra("totalScore"));
+        totalQRNumView.setText(intent.getStringExtra("totalQRCode"));
+        getAndSetRank(intent.getStringExtra("userID"));
+    }
+
+    public void getAndSetRank(String userID) {
+        PlayerCollection pc = new PlayerCollection(null);
+        CompletableFuture<Integer> rankFuture = pc.getPlayerRank(userID);
+        rankFuture.thenAccept(rank -> {
+            System.out.println("Player rank: " + rank);
+            rankView.setText(rank.toString());
+        }).exceptionally(e -> {
+            System.err.println("Failed to get player rank: " + e.getMessage());
+            return null;
+        });
     }
 }

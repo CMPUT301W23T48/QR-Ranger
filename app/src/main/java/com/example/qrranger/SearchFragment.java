618d3452cf0b01bae1daba4a8838d544071a14b0
Benny <BenCode.git@gmail.com>
2023-03-11 17:43:55 -0700

    final xml fix

--- app/src/main/java/com/example/qrranger/SearchFragment.java
+++ app/src/main/java/com/example/qrranger/SearchFragment.java
@@ -18,26 +18,13 @@ import java.util.ArrayList;
 
 public class SearchFragment extends Fragment{
     SearchView SearchUser;
-    ListView Users;
-    ArrayAdapter<String> UserAdapter;
-    ArrayList<String> dataList = new ArrayList<String>();
-
-
-    Activity OU_result;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_search, container, false);
-        Users = (ListView) view.findViewById(R.id.SearchUserList);
-        UserAdapter = new ArrayAdapter<>(getActivity(), android.R.layout.simple_list_item_1,dataList);
-        Users.setAdapter(UserAdapter);
         return view;
-    }
-    public void onStart(){
-        super.onStart();
-
     }
 
 

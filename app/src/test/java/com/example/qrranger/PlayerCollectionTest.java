d7075ad33070f975cd0107187fa38b9d508678b8
minhalo113 <115437688+minhalo113@users.noreply.github.com>
2023-03-13 08:53:46 -0600

    dd

--- app/src/test/java/com/example/qrranger/PlayerCollectionTest.java
+++ app/src/test/java/com/example/qrranger/PlayerCollectionTest.java
@@ -1,7 +1,90 @@
 package com.example.qrranger;
 
-import junit.framework.TestCase;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
 
-public class PlayerCollectionTest extends TestCase {
+import com.google.firebase.firestore.auth.User;
 
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Objects;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.TimeUnit;
+
+public class PlayerCollectionTest {
+
+    private static final int TIMEOUT_SECONDS = 5;
+
+    @Test
+    public void testCreate() throws InterruptedException {
+        Database db = Database.getInstance();
+        PlayerCollection pc = new PlayerCollection(db);
+
+        // create a map with sample data
+        Map<String, Object> values = new HashMap<>();
+        values.put("userID", "minh");
+        values.put("username", "minh123");
+        values.put("phoneNumber", "1234567890");
+        values.put("email", "minh@example.com");
+        values.put("geolocation_setting", true);
+        values.put("totalScore", 0);
+        values.put("totalQRCode", 0);
+
+        // create a countdown latch to wait for completion of the create operation
+        CountDownLatch latch = new CountDownLatch(1);
+
+        // create the document and wait for completion
+        pc.create(values);
+
+        boolean completed = latch.await(TIMEOUT_SECONDS, TimeUnit.SECONDS);
+
+        // check that the create operation completed successfully
+        assertTrue(completed);
+    }
+
+    public void read_test() throws InterruptedException{
+        Database db = Database.getInstance();
+        PlayerCollection pc = new PlayerCollection(db);
+
+        Player myTestUser = new Player();
+        UserState testUserState = UserState.getInstance();
+        String testStringID = testUserState.getUserID();
+
+        pc.read(testStringID, data -> {
+            // player found so handle code using data here
+            myTestUser.setUserName(Objects.requireNonNull(data.get("username")).toString());
+            myTestUser.setEmail(Objects.requireNonNull(data.get("email")).toString());
+            myTestUser.setPhoneNumber(Objects.requireNonNull(data.get("phoneNumber")).toString());
+            myTestUser.setTotalScore(((Long) data.get("totalScore")));
+            myTestUser.setTotalQRCode(((Long) data.get("totalQRCode")));
+            myTestUser.setGeoLocationSett((Boolean) data.get("geolocation_setting"));
+            myTestUser.setPlayerId(testStringID);
+            myTestUser.setQrCodeCollection((ArrayList<QRCode>) data.get("qr_code_ids"));
+        }, error -> {
+
+            System.out.println("Error getting player data: " + error);
+        });
+
+        // create a countdown latch to wait for completion of the read operation
+        CountDownLatch latch = new CountDownLatch(1);
+
+        // check if value read
+        assertTrue(myTestUser.getUserName() != null );
+        assertTrue(myTestUser.getEmail()!= null);
+        assertTrue(myTestUser.getPlayerId() != null);
+        assertTrue(myTestUser.getPhoneNumber()!= null);
+        assertTrue(myTestUser.getTotalQRCode()!= null);
+
+        boolean completed = latch.await(TIMEOUT_SECONDS, TimeUnit.SECONDS);
+        System.out.println("completed");
+        // check that the create operation completed successfully
+        assertTrue(completed);
+    }
 }
+
+
+
+
--- app/src/test/java/com/example/qrranger/QRCollectionTest.java
+++ app/src/test/java/com/example/qrranger/QRCollectionTest.java
@@ -2,7 +2,38 @@ package com.example.qrranger;
 
 import junit.framework.TestCase;
 
-public class QRCollectionTest extends TestCase {
+import org.junit.Test;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.TimeUnit;
+
+public class QRCollectionTest extends TestCase {
+    private static final int TIMEOUT_SECONDS = 5;
+    @Test
+    public void testCreate() throws InterruptedException{
+        Database db = Database.getInstance();
+        QRCollection qrCollection = new QRCollection(db);
+
+        // Create a Map of field-value pairs for the new document
+        Map<String, Object> values = new HashMap<>();
+        values.put("qr_id", "123");
+        values.put("name", "John");
+        values.put("url", "mjkkkkkkkk");
+        values.put("points", 10);
+
+        // create a countdown latch to wait for completion of the create operation
+        CountDownLatch latch = new CountDownLatch(1);
+
+        // Call the create() method and verify that a new document is added to the collection
+        boolean completed = latch.await(TIMEOUT_SECONDS, TimeUnit.SECONDS);
+        qrCollection.create(values);
+        CompletableFuture<Boolean> check = qrCollection.checkQRExists("123");
+
+        assertTrue(completed);
+        assertTrue(check == CompletableFuture.completedFuture(true));
+    }
 
 }

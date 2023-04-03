dd26e2bccb058ef10f3ac4242fe31da332616289
Jefferson Fong <jefferso@ualberta.ca>
2023-04-02 23:13:46 -0600

    Create deploymentTargetDropDown.xml
    Test

--- app/src/main/java/com/example/qrranger/MapFragment.java
+++ app/src/main/java/com/example/qrranger/MapFragment.java
@@ -1,20 +1,179 @@
 package com.example.qrranger;
 
+
+import android.Manifest;
+
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
 import android.os.Bundle;
-
-import androidx.fragment.app.Fragment;
-
+import android.provider.Settings;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import androidx.core.content.ContextCompat;
+import androidx.fragment.app.Fragment;
 
 
-public class MapFragment extends Fragment {
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapView;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+import com.karumi.dexter.Dexter;
+import com.karumi.dexter.PermissionToken;
+import com.karumi.dexter.listener.PermissionDeniedResponse;
+import com.karumi.dexter.listener.PermissionGrantedResponse;
+import com.karumi.dexter.listener.PermissionRequest;
+import com.karumi.dexter.listener.single.PermissionListener;
+import android.content.Context;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class MapFragment extends Fragment implements OnMapReadyCallback{
+
+
+    //Initialize Variables
+    public MapView mapView;
+    private GoogleMap gMap;
+
+    private boolean isPermissionGranted;
+
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_map, container, false);
+        View view = inflater.inflate(R.layout.fragment_map, container, false);
+
+        //Important to get map working
+//        Bundle mapViewBundle = null;
+//        if (savedInstanceState!=null){
+//            mapViewBundle = savedInstanceState.getBundle("MapViewBundleKey");
+//        }
+
+        mapView = (MapView) mapView.findViewById(R.id.mapView);
+        mapView.onCreate(savedInstanceState);
+
+        //Checks if user gives permission to track device
+        /*
+        If permission is given, then google maps can track user location
+         */
+        deviceLocationCheckPermission();
+
+
+
+        // Call the init method and pass the inflated View as a parameter
+        return view;
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        gMap = googleMap;
+
+        //Add a marker and move the camera
+        //Zoom in on Players current location
+        LatLng home = new LatLng(53.62619228224998, -113.43941918391658);
+        gMap.moveCamera(CameraUpdateFactory.newLatLngZoom(home, 15));
+        gMap.addMarker(new MarkerOptions().position(home).title("This is my home"));
+
+        /*
+        Checks if the Permission is granted
+         */
+
+        if (ContextCompat.checkSelfPermission(this.getContext(), Manifest.permission.ACCESS_FINE_LOCATION)
+                == PackageManager.PERMISSION_GRANTED){
+            gMap.setMyLocationEnabled(true);
+        }
+
+    }
+
+    public void initMap(){
+        mapView.getMapAsync(this);
+    }
+
+
+
+    //Check user Permission to access the map
+
+
+    public void init(View view){
+
+    }
+
+    //Function for getting device location permission
+    private void deviceLocationCheckPermission(){
+        Dexter.withContext(this.getContext()).withPermission(Manifest.permission.ACCESS_FINE_LOCATION).withListener(new PermissionListener() {
+            @Override
+            public void onPermissionGranted(PermissionGrantedResponse permissionGrantedResponse) {
+                //Initialize the map on permissionGranted
+                initMap();
+
+            }
+
+            @Override
+            public void onPermissionDenied(PermissionDeniedResponse permissionDeniedResponse) {
+                Intent intent = new Intent();
+                intent.setAction(Settings.ACTION_APPLICATION_DETAILS_SETTINGS);
+                Uri uri=Uri.fromParts("package", getContext().getPackageName(),"");
+                intent.setData(uri);
+                startActivity(intent);
+            }
+            @Override
+            public void onPermissionRationaleShouldBeShown(PermissionRequest permissionRequest, PermissionToken permissionToken) {
+                permissionToken.continuePermissionRequest();
+            }
+        }).check();
+    }
+
+    //Search Location Function
+    public void searchLocation(){
+
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        mapView.onStart();
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        mapView.onStop();
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        mapView.onResume();
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        mapView.onPause();
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        mapView.onDestroy();
+    }
+
+    @Override
+    public void onLowMemory() {
+        super.onLowMemory();
+        mapView.onLowMemory();
+    }
+
+    @Override
+    public void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        mapView.onSaveInstanceState(outState);
     }
 }
